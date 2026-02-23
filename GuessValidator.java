cb5ad7a616af5776531eedd500318004780d217e
merock2024 <merock2024@gmail.com>
2026-02-23 15:36:12 +0530

    Coded the UC2 that allows a Guess Validation method.

---
+++ GuessValidator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class GuessValidator {
+
+    public static String validateGuess(int guess, int target) {
+        if(guess == target) {
+            return "CORRECT";
+        }
+        else if (guess < target){
+            return "LOW";
+        }
+        return "HIGH";
+    }
+
+}
--- GuessingApp.java
+++ GuessingApp.java
@@ -1,11 +1,9 @@
-
 /*
-UC1 - Game Initialization
+UC2 - Comparing User Guess Submission
 
-This class sets the Game Boundaries, generate a random target number and display 
-the game rules.
-It demonstrates Encapsulation, constructor initialisation and random number generation.
+This is responsible for comparing User's guess with the target number.
  */
+
 import java.util.*;
 
 public class GuessingApp{
@@ -13,6 +11,23 @@ public class GuessingApp{
         System.out.println("Welcome to the Guessing App!");
         GameConfig gameConfig = new GameConfig();
         gameConfig.showRules();
+
+        Scanner sc = new Scanner(System.in);
+        int attempts = 0;
+
+        while (attempts < gameConfig.getMaxAttempts()) {
+            System.out.print("Enter your guess: ");
+            int guess = sc.nextInt();
+            attempts++;
+
+            String result = GuessValidator.validateGuess(guess, gameConfig.getTargetNumber());
+
+            System.out.println(result);
+
+            if ("CORRECT".equals(result)) {
+                break;
+            }
+        }
 
     }
 }
